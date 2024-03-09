df8185687e141623d4af6ba54cf7a25fc975a50b
JSMarsh813 <janetspellman13@gmail.com>
2024-03-08 22:26:31 -0800

    added sitemap package and created sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import purgecss from "astro-purgecss";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), purgecss()]
+  site: "https://ufancy.netlify.app/",
+  integrations: [react(), tailwind(), purgecss(), sitemap()],
 });
