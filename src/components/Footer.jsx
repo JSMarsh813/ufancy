78e6a0ab051f2bd0c958da09e59cae92f511363a
JSMarsh813 <janetspellman13@gmail.com>
2024-03-08 21:36:32 -0800

    changed footer copyright text from grey to black, improving contrast

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,7 +64,7 @@ export const Footer = () => {
               </div>
             </div>
           </div>
-          <p className="lg:text-center text-sm text-gray-400 border-t border-[rgb(255,255,255,0.2)] pt-12 mt-16 hidden lg:block">
+          <p className="lg:text-center text-sm text-black border-t border-[rgb(255,255,255,0.2)] pt-12 mt-16 hidden lg:block">
             &copy; 2024. MIT license.
           </p>
         </div>
